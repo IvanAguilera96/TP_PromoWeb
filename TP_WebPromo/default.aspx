f3b3e1cfff986e979f2a1f2f58017fb6b4a3ebb9
Ivan Eloy Aguilera <ivaneaguilera96@gmail.com>
2026-05-09 19:43:50 -0300

    Creacion de master, pagina defualt y diseño estructura primer pagina con master

---
+++ TP_WebPromo/default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="TP_WebPromo._default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Label Text="Ingresá el codigo de tu voucher!" ID ="lblText" runat="server" />
+    <br />
+    <asp:TextBox id="txtText" runat="server" />
+    <br />
+    <asp:Button Text="Siguiente" ID="btnSiguiente" class="btn btn-primary btn-sm" runat="server" />
+</asp:Content>
---
+++ TP_WebPromo/default.aspx.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TP_WebPromo
+{
+
+
+    public partial class _default
+    {
+
+        /// <summary>
+        /// Control lblText.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblText;
+
+        /// <summary>
+        /// Control txtText.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtText;
+
+        /// <summary>
+        /// Control btnSiguiente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSiguiente;
+    }
+}
